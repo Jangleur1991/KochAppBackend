f24e97dd4618f8424787736ed378abf3fe612eec
Jangleur1991 <Jangleur1991@web.de>
2022-03-03 09:56:06 +0000

    Add missing thingsAdd missing things

---
+++ KochAppBackend/src/main/java/de/kochAppBackend/KochAppBackend/KochAppBackendApplication.java
@@ -0,0 +1,13 @@
+package de.kochAppBackend.KochAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KochAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KochAppBackendApplication.class, args);
+	}
+
+}

b25341508d7cba870504b96f80a9a52caf5b2e86
Jan Albert <j.albert@teckpro.de>
2022-03-11 10:20:32 +0100

    Removed field injection, introduced intermediate layer RecipeService

--- KochAppBackend/src/main/java/de/kochAppBackend/KochAppBackend/controller/RecipeController.java
+++ KochAppBackend/src/main/java/de/kochAppBackend/KochAppBackend/controller/RecipeController.java
@@ -3,46 +3,44 @@ package de.kochAppBackend.KochAppBackend.controller;
 import de.kochAppBackend.KochAppBackend.model.RecipeRequest;
 import de.kochAppBackend.KochAppBackend.model.RecipeResponse;
 import de.kochAppBackend.KochAppBackend.repository.RecipeRepository;
+import de.kochAppBackend.KochAppBackend.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
-import static de.kochAppBackend.KochAppBackend.validations.RecipeValidation.isValid;
-
+/*
+    RestController: Process requests and generate responses. Part of the WebLayer!
+ */
 @RestController
 public class RecipeController {
 
+    private final RecipeRepository recipeRepository;
+
+    RecipeService recipeService = new RecipeService();
+
     @Autowired
-    RecipeRepository recipeRepository;
+    public RecipeController(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
 
     @GetMapping("/recipes")
     public List<RecipeResponse> getAllRecipes(@RequestParam(required = false) String tag) {
-        return recipeRepository.findAll(tag);
+        return recipeService.getAllRecipes(tag);
     }
 
     @GetMapping("/recipes/{id}")
     public ResponseEntity<RecipeResponse> getRecipeById(@PathVariable String id) {
-        return recipeRepository //
-                .findById(id) //
-                .map(ResponseEntity::ok) //StatusCode 200
-                .orElseGet(() -> ResponseEntity.notFound().build()); //StatusCode 404 in case id doesn't exist
+        return recipeService.getRecipeById(id);
     }
 
     @PostMapping("/recipes")
     public ResponseEntity<RecipeResponse> createProduct(@RequestBody RecipeRequest request) {
-        if (isValid(request)) {
-            recipeRepository.save(request);
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        return recipeService.createProduct(request);
     }
 
     @DeleteMapping("/recipes/{id}")
     public ResponseEntity<RecipeResponse> deleteRecipe(@PathVariable String id) {
-        recipeRepository.deleteById(id);
-        return ResponseEntity.noContent().build(); // If there is no content, you return noContent 204
+        return recipeService.deleteRecipe(id); // If there is no content, you return noContent 204
     }
 }

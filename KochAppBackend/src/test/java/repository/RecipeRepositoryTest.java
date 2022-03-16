566eb420104cc9e4b7833f924a1c5b322faa2021
Jan Albert <j.albert@teckpro.de>
2022-03-16 13:05:08 +0100

    Added testThatSaveWithRequestReturnResponse

--- KochAppBackend/src/test/java/repository/RecipeRepositoryTest.java
+++ KochAppBackend/src/test/java/repository/RecipeRepositoryTest.java
@@ -3,23 +3,28 @@ package repository;
 import de.kochAppBackend.KochAppBackend.model.RecipeRequest;
 import de.kochAppBackend.KochAppBackend.model.RecipeResponse;
 import de.kochAppBackend.KochAppBackend.repository.RecipeRepository;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class RecipeRepositoryTest {
+
+    private static final String NAME = "testname";
+    private static final String DURATION_IN_MINUTES = "20";
+    private static final String DESCRIPTION = "Cook for 15 Minutes";
+    private static final List<String> INGREDIENTS = List.of("tomatoes", "salad");
+    private static final List<String> TAGS = List.of("healthy");
 
     private RecipeRepository testCandidate;
 
@@ -132,22 +137,25 @@ public class RecipeRepositoryTest {
     }
 
     @Test
-    public void testThatSaveWithRequestNameReturnsResponseName() {
+    public void testThatSaveWithRequestReturnResponse() {
         //given
-        String name = "testname";
         RecipeRequest request = new RecipeRequest(
-                name,
-                null,
-                null,
-                null,
-                null
+                NAME,
+                DURATION_IN_MINUTES,
+                DESCRIPTION,
+                INGREDIENTS,
+                TAGS
         );
 
         //when
         RecipeResponse result = testCandidate.save(request);
 
         //then
-        assertThat(result.getName()).isEqualTo(name);
+        assertThat(result.getName()).isEqualTo(NAME);
+        assertThat(result.getDurationInMinutes()).isEqualTo(DURATION_IN_MINUTES);
+        assertThat(result.getDescription()).isEqualTo(DESCRIPTION);
+        assertTrue(CollectionUtils.isEqualCollection(INGREDIENTS, result.getIngredients()));
+        assertTrue(CollectionUtils.isEqualCollection(TAGS, result.getTags()));
     }
 
 }
